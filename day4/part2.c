d20bd04458e6a66beebcae958ef00ead958796ec
Sam Bessey <sam_bessey@brown.edu>
2022-12-04 13:44:04 -0500

    day4

---
+++ day4/part2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char line[255];
+    FILE* fptr;
+    int pairs = 0;
+
+    if ((fptr = fopen("data.txt", "r")) == NULL) {
+        printf("ERROR: Could not open file");
+        exit(1);
+    }
+    while (fgets(line, 255, fptr)) {
+        char* token1 = strtok(line, ",");
+        char* token2 = strtok(NULL, ",");
+
+        int one_first = atoi(strtok(token1, "-"));
+        int one_second = atoi(strtok(NULL, "-"));
+        int two_first = atoi(strtok(token2, "-"));
+        int two_second = atoi(strtok(NULL, "-"));
+
+        if ((one_first >= two_first && one_first <= two_second) || 
+                (one_second <= two_second && one_second >= two_first) || 
+                (one_second == two_first) || 
+                (one_first == two_second) ||
+                (one_first <= two_first && one_second >= two_second) ||
+                (one_first >= two_first && one_second <= two_second)
+            ) {
+            pairs++;
+        }
+    }
+    printf("%d", pairs);
+    return 0;
+}
