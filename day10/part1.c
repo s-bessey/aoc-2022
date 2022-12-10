9aa7167ea91593258b5ecf14a5542b1134ce2088
Sam Bessey <sam_bessey@brown.edu>
2022-12-10 12:12:11 -0500

    day10 WIP

---
+++ day10/part1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char line[16];
+    FILE* fptr;
+
+    if ((fptr = fopen("example.txt", "r")) == NULL) {
+        printf("ERROR: Could not open file");
+        exit(1);
+    }
+    // store the current cycle, the total value, and the current signal
+    int value = 0;
+    int signal = 1;
+    int cycle = 0;
+    while (fgets(line, 16, fptr)){
+        //  get the instruction and value
+        char delimiter[] = " ";
+        char *op = strtok(line, delimiter);
+        int v = atoi(strtok(NULL, delimiter));
+
+        // if no operation, just check if we're in a cycle we want
+        if (strcmp(op, "noop") == 0){
+            if ((cycle - 20) % 40 == 0 && cycle < 221){
+                value += (cycle * signal);
+                printf("%d %d %d\n", value, cycle, signal);
+            }
+            cycle++;
+
+        } else {
+            // Otherwise, check this cycle, increment, then check again
+            if ((cycle - 20) % 40 == 0 && cycle < 221){
+                value += (cycle * signal);
+                printf("%d %d %d\n", value, cycle, signal);
+            }
+            cycle++;
+            if ((cycle - 20) % 40 == 0 && cycle < 221){
+                value += (cycle * signal);
+                printf("%d %d %d\n", value, cycle, signal);
+            }
+            cycle++;
+            // add value to signal
+            signal += v;
+        }
+    }
+    printf("part1: %d\n", value);
+}
+        
+
