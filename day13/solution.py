0b82d69d8f45af277ade3a2c6e3c9b198a42e16b
Sam Bessey <sam_bessey@brown.edu>
2023-02-03 12:41:01 -0500

    WIP: day 13

--- day13/solution.py
+++ day13/solution.py
@@ -12,19 +12,19 @@ def receive_signal(filename):
 def compare_numbers(left, right):
     print(left, right)
     if left == right:
-        return 0
-    if left > right:
         return -1
+    if left > right:
+        return 0
     if left < right:
         return 1    
 
 def compare_packets(left, right):
     if not left and not right:
-        return -1
+        return 0
     elif not left:
         return 1
     elif not right:
-        return -1
+        return 0
     if not isinstance(left, list):
         left = [left]
     elif type(right) != list:
@@ -32,16 +32,17 @@ def compare_packets(left, right):
     if isinstance(left[0], int) and isinstance(right[0], int):
         for i in range(len(left)):
             correct = compare_numbers(left, right)
-            if correct != 0:
+            if correct != -1:
+                print(correct)
                 return correct
     for i in range(min(len(left), len(right))):
         if isinstance(left[i], list) or isinstance(right[i], list):
             compare_packets(left[i], right[i])
         else:
             correct = compare_numbers(left[i], right[i])
-            if correct != 0:
+            if correct != -1:
                 return correct
-    
+
 
 
 # print(receive_signal("day13\example.txt"))
